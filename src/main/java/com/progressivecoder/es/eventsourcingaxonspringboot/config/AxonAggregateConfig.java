7913b2b359e8aee0ed7dfd7c144236620e113e3d
ifeoluwa5983 <o.ifeoluwah@gmail>
2021-05-04 10:28:08 +0100

    initial commit

--- src/main/java/com/progressivecoder/es/eventsourcingaxonspringboot/config/AxonAggregateConfig.java
+++ src/main/java/com/progressivecoder/es/eventsourcingaxonspringboot/config/AxonAggregateConfig.java
@@ -1,11 +1,17 @@
 package com.progressivecoder.es.eventsourcingaxonspringboot.config;
 
 
+import com.mongodb.MongoClient;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.mongo.DefaultMongoTemplate;
+import org.axonframework.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//public class AxonAggregateConfig {
-//    @Bean
-//    public EventStorageEngine eventStore(MongoClient client) {
-//        return new MongoEventStorageEngine(new DefaultMongoTemplate(client));
-//    }
-//}
+@Configuration
+public class AxonAggregateConfig {
+    @Bean
+    public EventStorageEngine eventStore(MongoClient client) {
+        return new MongoEventStorageEngine(new DefaultMongoTemplate(client));
+    }
+}
